4359c6305287b56f15f61ebf3af33b4d75cdb59b
DAST-OfficePC <jackie@dast.tw>
2023-08-10 12:30:13 +0800

    refactor: refactor variable names and add comments for user ID and group ID
    - Change the variable name `UID` to `uid`
    - Change the variable name `GID` to `gid`
    - Change the variable name `USER` to `user`
    - Add a comment to explain the purpose of setting the user ID and group ID
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- src/start.sh
+++ src/start.sh
@@ -5,6 +5,11 @@ if [ "${UMASK:-UNSET}" != "UNSET" ]; then
   umask "$UMASK"
 fi
 
+# set User ID & Group ID
+UID = (id -u)
+GID = (id -g)
+USER = ${UID}:${GID}
+
 # Create client_login if it doesn't exist yet
 if [ ! -f /hath/data/client_login ]; then
 	printf "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /hath/data/client_login
