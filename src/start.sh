ad204dfbb3bc4ade942cf3353ae9b3dc7287010b
DAST-OfficePC <jackie@dast.tw>
2023-08-10 12:37:06 +0800

    chore: remove UID and GID configuration lines
    - Remove the lines setting the UID and GID
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- src/start.sh
+++ src/start.sh
@@ -5,10 +5,6 @@ if [ "${UMASK:-UNSET}" != "UNSET" ]; then
   umask "$UMASK"
 fi
 
-# set User ID & Group ID
-UID =(id -u)
-GID =(id -g)
-
 # Create client_login if it doesn't exist yet
 if [ ! -f /hath/data/client_login ]; then
 	printf "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /hath/data/client_login
