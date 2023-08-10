065b088bcb330e783496995c3cb0319241b420a1
DAST-OfficePC <jackie@dast.tw>
2023-08-10 12:00:25 +0800

    chore: improve User and Group ID retrieval and client login file creation
    - Add code to get User ID and Group ID
    - Create `client_login` file if it doesn't exist yet
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- src/start.sh
+++ src/start.sh
@@ -5,6 +5,9 @@ if [ "${UMASK:-UNSET}" != "UNSET" ]; then
   umask "$UMASK"
 fi
 
+# Get User ID & Group ID
+uid=$(id -u) gid=$(id -g)
+
 # Create client_login if it doesn't exist yet
 if [ ! -f /hath/data/client_login ]; then
 	printf "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /hath/data/client_login
