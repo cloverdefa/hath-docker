4068349d14e6a8569e604c2aadc6e7f96173cbcd
Macbook <jackie@dast.tw>
2024-02-27 17:55:12 +0800

    重構：重構目錄創建邏輯
    - 更改目錄創建邏輯，以便分別檢查並創建每個目錄
    
    Signed-off-by: Macbook <jackie@dast.tw>

--- start/run.sh
+++ start/run.sh
@@ -7,7 +7,11 @@ set -e
 [ "${UMASK:-UNSET}" != "UNSET" ] && umask "$UMASK"
 
 # 檢查並建立缺少的資料夾
-mkdir -p /hath/cache /hath/data /hath/download /hath/log /hath/tmp
+[ -d /hath/cache ] || mkdir -p /hath/cache
+[ -d /hath/data ] || mkdir -p /hath/data
+[ -d /hath/download ] || mkdir -p /hath/download
+[ -d /hath/log ] || mkdir -p /hath/log
+[ -d /hath/tmp ] || mkdir -p /hath/tmp
 
 # 如果 client_login 檔案不存在，則創建並寫入內容
 if [ ! -f /hath/data/client_login ]; then
